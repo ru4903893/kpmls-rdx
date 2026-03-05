9f387f9c82bb9a97ab6a9ef82bc29c39f863769d
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2026-03-05 11:07:00 +0400

    Create multi_host_extractor.py

---
+++ bot/helper/ext_utils/multi_host_extractor.py
@@ -0,0 +1,30 @@
+import re
+from cloudscraper import create_scraper
+
+SUPPORTED_HOSTS = [
+    "drive.google.com",
+    "pixeldrain.com",
+    "mega.nz",
+    "mediafire.com",
+    "gofile.io"
+]
+
+def extract_mirrors(url):
+    try:
+        scraper = create_scraper()
+        r = scraper.get(url, timeout=20)
+
+        html = r.text
+        links = []
+
+        for host in SUPPORTED_HOSTS:
+            pattern = r'https?://[^"\\']*' + re.escape(host) + r'[^"\\']*'
+            matches = re.findall(pattern, html)
+            links.extend(matches)
+
+        links = list(set(links))
+
+        return links
+
+    except Exception:
+        return []
